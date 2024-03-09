26f172c3a73eee35eb20992a456190a1f321a40d
Neul Seo <neul.seo.1213@gmail.com>
2024-03-09 14:59:30 -0500

    created controller and router files and error handlers in server.js

---
+++ server/controllers/petLoggerController.js
@@ -0,0 +1,20 @@
+//  DOG {
+// 	DogID: integer,
+// 	Name: string,
+// 	Age: number,
+// 	Breed: string
+// 	gender: string,
+// 	Users: [userID: integer]
+// 	Posts: [{type: string, details: string, date:date}]
+// }
+
+// GET REQUEST TO LOAD ALL DEPENDENTS. Info needed: name and image and id. Type for response : ARRAY of dog objects
+// Method: GET
+// Endpoint: /dog
+// ControllerMethod:
+// Response format:
+
+// Method: POST
+// Endpoint: localhost:3000/dog
+// Body: { name, breed, age, gender }
+// Controller Method: addDog
