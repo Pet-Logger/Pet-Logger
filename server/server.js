702542d94616dcfdc8bb9e5433aec169e3434a48
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-03-09 14:45:35 -0500

    mongoose.connect useNewUrlParser didnt work-deprecated

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-const path = require('path'); 
+const path = require('path');
 const express = require('express');
+const models = require('./models/petLoggerModels');
 
-const app = express(); 
-const PORT = 3000; 
+const app = express();
+const PORT = 3000;
 
 
 if (process.env.NODE_ENV === 'development') {
@@ -11,10 +12,24 @@ if (process.env.NODE_ENV === 'development') {
   app.use(express.static(path.resolve(__dirname, '../dist')));
 }
 
+async function createUser() {
+  try {
+    const newUser = await models.User.create({ name: 'Bongi', userName: 'petLoggerBongi', password: '12345' });
+    console.log(newUser);
+  }
+  catch (error) {
+
+    console.log(error);
+  }
+}
+
+createUser();
+// const user = await models.User.findOne({ name: 'Bongi' });
+// console.log({ user });
 
 
-app.listen(PORT, ()=> {
-  console.log(`server listening on port ${PORT}: http://localhost:${PORT}/`); 
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}: http://localhost:${PORT}/`);
 })
 
 module.exports = app; 
