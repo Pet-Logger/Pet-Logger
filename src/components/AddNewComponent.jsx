f16407ce9d95ec88ced1d0627979d108dedd3592
Reem <abiammar.reem@gmail.com>
2024-03-09 18:16:50 -0300

    completed form functionality

--- src/components/AddNewComponent.jsx
+++ src/components/AddNewComponent.jsx
@@ -1,36 +1,72 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 
 const breedArray = ['Husky', 'Golden Retreiver', 'Dalmatian', 'German Shepard', 'Lab', 'Beagle', 'Poodle']
 
+const useInput = init => {
+  const [ value, setValue ] = useState(init);
+  const onChange = e => {
+    setValue(e.target.value);
+  };
+  return [ value, onChange ];
+};
 
 const AddNewComponent = () => {
+  const [ name, nameOnChange ] = useInput('');
+  const [ age, ageOnChange ] = useInput('');
+  const [ breed, breedOnChange ] = useInput('');
+  const [ gender, genderOnChange ] = useInput('');
+  
+  
+  const saveCharacter = () => {
+    // check if name is empty
+      const body = {
+        name,
+        age,
+        breed,
+        gender,
+      };
+      console.log("req.body", body)
 
-  function createDependent(){
-    console.log("to be continued"); 
-  }
-
+  //     fetch('/api/character', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'Application/JSON'
+  //       },
+  //       body: JSON.stringify(body)
+  //     })
+  //       .then(resp => resp.json())
+  //       .then(data => {
+  //         console.log(data);
+  //       })
+  //       .then(() => {
+  //         props.history.push('/');
+  //       })
+  //       .catch(err => console.log('CreateCharacter fetch /api/character: ERROR: ', err));
+  //   }
+  };
 
   return (
     <div className='addnewcomponent'>
       <h3 className='newdependentname'><Link to="/user">This is new dependent form</Link></h3>
       <form>
-        <label htmlFor="fname">Name:</label>
-        <input type="text" id="name" ></input>
-        <label htmlFor="fname">Age:</label>
-        <input type="text" id="age" ></input>
-        <label htmlFor="fname">Breed:</label>
-        <select id="breed">
+        <label htmlFor="name" >Name</label>
+        <input type="text" id="name" name="name" value={name} onChange={nameOnChange}></input>
+        <label htmlFor="breed" >Age: </label>
+        <input type="text" id="age" name="age" value={age} onChange={ageOnChange}></input>
+        <label htmlFor="breed" >Breed:</label>
+        <select id="breed" name="breed" value={breed} onChange={breedOnChange}>
           <option value="Husky">Husky</option>
           <option value="Dalmation">Dalmation</option>
         </select> 
-        <label htmlFor="fname">Gender:</label>
-        <select id="gender">
+        <label htmlFor="gender">Gender:</label>
+        <select id="gender" name="gender" value={gender} onChange={genderOnChange}>
           <option value="male">Male</option>
           <option value="female">Female</option>
         </select> 
-        <button type='submit' onClick={()=>createDependent()}> Submit</button>
+        <button type='button' onClick={saveCharacter}> Submit</button>
       </form>
       
 
