6ce00341a9990a93befd1e89f516be7807e4dcb8
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-03-09 15:34:24 -0500

    Updated and tested petLogger model

--- server/server.js
+++ server/server.js
@@ -12,10 +12,11 @@ if (process.env.NODE_ENV === 'development') {
   app.use(express.static(path.resolve(__dirname, '../dist')));
 }
 
-async function createUser() {
+async function createUserAndDog() {
   try {
     const newUser = await models.User.create({ name: 'Bongi', userName: 'petLoggerBongi', password: '12345' });
-    console.log(newUser);
+    const newDog = await models.Dog.create({ name: 'dog1', age: 23, breed: 'Spaniel', gender: 'F', users: [{ userId: '65ecbe30d6da6de8222431e2' }]});
+    console.log({newUser}, {newDog});
   }
   catch (error) {
 
@@ -23,13 +24,21 @@ async function createUser() {
   }
 }
 
-createUser();
-// const user = await models.User.findOne({ name: 'Bongi' });
-// console.log({ user });
+async function findUserAndDog(){
+  try {
+    const foundDog = await models.Dog.find({name: 'dog1'});
+    const foundUser = await models.User.find({name: 'Bongi'});
+    console.log({foundDog}, {foundUser});
+  } catch (error) {
+    console.log(error);
+  }
+}
+createUserAndDog();
+findUserAndDog();
 
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}: http://localhost:${PORT}/`);
-})
+});
 
 module.exports = app; 
