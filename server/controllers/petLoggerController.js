4c9459ee5dff60217fc4e232138dde246748930f
Neul Seo <neul.seo.1213@gmail.com>
2024-03-09 16:29:23 -0500

    added error handler for POST request and added petLoggerModels

--- server/controllers/petLoggerController.js
+++ server/controllers/petLoggerController.js
@@ -18,16 +18,34 @@
 // Endpoint: localhost:3000/dog
 // Body: { name, breed, age, gender }
 // Controller Method: addDog
-
+const model = require('../models/petLoggerModels');
 const petLoggerController = {};
 
 // GET: middleware for retreiving a dog data
 petLoggerController.getDog = (req, res, next) => {};
 
 // POST: middleware for adding a new dog
-petLoggerController.addDog = (req, res, next) => {
-  const { name, breed, age, gender } = req.body;
-  next();
+petLoggerController.addDog = async (req, res, next) => {
+  try {
+    // declare the constants we're going to use
+    const { name, breed, age, gender } = req.body;
+
+    // async connect to the mongo DB, and create a new dog
+    const newDog = await model.Dog.create({ name, breed, age, gender });
+
+    // save the dog object in res.locals as newDog
+    res.locals.newDog = newDog;
+
+    return next();
+  } catch (err) {
+    return next({
+      log: `Error in getDog middleware ${err}`,
+      status: 500,
+      message: `Error in getDog middleware`,
+    });
+  }
+
+  // handle errors
 };
 
 module.exports = petLoggerController;
