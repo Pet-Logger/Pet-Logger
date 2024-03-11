42c7fc85c02e7be616c429d9d40dd6f306d6ac34
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-03-11 10:40:33 -0400

    pushing everything

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+
+import MainContainer from "./containers/MainContainer";
+import UserProfileContainer from "./containers/UserProfileContainer";
+import DependentContainer from "./containers/DependentContainer";
+import AddNewComponent from "./components/AddNewComponent";
+
+
+
+const App = () => {
+  return (
+    <div>Hello world!
+      {/* <p>Michael was up in here!</p>
+      <p>This is Reem</p>
+      <p>Hello Guys Bongi</p> */}
+      <main>
+      <MainContainer/>
+
+        <Routes>
+          <Route path = 'user' element ={<UserProfileContainer/>} />
+          <Route path = 'dependent' element={<DependentContainer/>}/>
+          <Route path = 'addnew' element={<AddNewComponent/>}/>
+        </Routes>
+
+
+
+      {/* </MainContainer> */}
+      </main>
+    </div>
+    
+  );
+};
+
+export default App;
