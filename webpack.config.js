837f6c0ecad6ab1e9fb9750620ae9a6d5d183556
Howard Sun <komugi@gmail.com>
2024-03-12 14:17:38 -0400

    Streamlined webpack and updated package.json scripts Updated .gitignore and removed redundancy in server.js for static path
    On branch howard/fix-webpack
    Changes to be committed:
    	modified:   .gitignore
    	modified:   package.json
    	modified:   server/server.js
    	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,36 @@
-const webpack = require('webpack'); 
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', 
+  mode: process.env.NODE_ENV,
+
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js', 
+    filename: 'bundle.js',
   },
+
   devServer: {
     host: 'localhost',
     port: 8080,
-    hot: true, 
+    hot: true,
     static: {
-      directory: path.resolve(__dirname, 'dist')
+      directory: path.resolve(__dirname, 'dist'),
     },
-    proxy: [{
-      context: ['/api/**'],
-      target: 'http://localhost:3000/',
-    },
+    proxy: [
+      {
+        context: ['/api/**'],
+        target: 'http://localhost:3000/',
+      },
     ],
   },
+
   plugins: [
     new HTMLWebpackPlugin({
-      template: './src/index.html'
-    })
+      template: './src/index.html',
+    }),
   ],
+
   module: {
     rules: [
       {
@@ -34,20 +39,18 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/env', '@babel/react']
-          }
-        }
+            presets: ['@babel/env', '@babel/react'],
+          },
+        },
       },
       {
-        test: /\.s?css$/, 
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ],
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
-
-    ]
+    ],
   },
   resolve: {
     // Enable importing JS / JSX files without specifying their extension
     extensions: ['.js', '.jsx'],
   },
-
 };
