4d2664b703e29edf1fce2b353c2e0fba97c5e149
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-03-11 22:31:02 -0400

    eslint

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
-const path = require('path'); 
-const express = require('express');
+const path = require("path"); 
+const express = require("express");
 
 const app = express(); 
 
 const PORT = 3000; 
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(express.static(path.resolve(__dirname, '../src')));
+if (process.env.NODE_ENV === "development") {
+    app.use(express.static(path.resolve(__dirname, "../src")));
 } else {
-  app.use(express.static(path.resolve(__dirname, '../dist')));
+    app.use(express.static(path.resolve(__dirname, "../dist")));
 }
 
 
 
 app.listen(PORT, ()=> {
-  console.log(`server listening on port ${PORT}: http://localhost:${PORT}/`); 
-})
+    console.log(`server listening on port ${PORT}: http://localhost:${PORT}/`); 
+});
 
 module.exports = app; 
